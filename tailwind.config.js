018f2fa1023a26e44f6870277e323458436a06d7
Morteza Rahimi <zeamort@gmail.com>
2025-03-15 10:14:38 -0700

    setup tailwind styling

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#030014',
+        secondary: '#151312',
+        light: {
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB'
+        },
+        dark: {
+          100: '#221f3d',
+          200: '#0f0d23'
+        },
+        accent: '#AB8BFF'
+      }
+    },
+  },
+  plugins: [],
+}
