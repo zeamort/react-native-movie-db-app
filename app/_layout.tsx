d99a327d82be2946b287961acdf3d0e6a91cc5ae
Morteza Rahimi <zeamort@gmail.com>
2025-03-15 11:29:28 -0700

    implement scaffold of all app screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from "expo-router";
-import './globals.css';
+import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+    return (
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+        </Stack>
+    );
 }
